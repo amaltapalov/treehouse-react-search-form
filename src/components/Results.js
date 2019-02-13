b81ccf050e14b5f071a50794f44299bd68fa2944
amal <amal.t@tabella.cz>
2019-02-13 11:57:26 +0100

    first commit

---
+++ src/components/Results.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    Container, ListGroup, ListGroupItem
+} from 'react-bootstrap';
+
+const Results = () => (
+    <Container>
+        <h2>Results list</h2>
+        <ListGroup>
+          <ListGroupItem href="#" active>Link 1</ListGroupItem>
+          <ListGroupItem href="#">Link 2</ListGroupItem>
+          <ListGroupItem href="#" disabled>Link 3</ListGroupItem>
+        </ListGroup>
+    </Container>
+);
+
+export default Results;
